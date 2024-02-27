811e66d79e3da771d9d66045461bc98306dbbce8
pradeepbaddapuri09 <pradeepbaddapuri09@gmail.com>
2024-02-27 21:27:07 +0530

    Home Page text update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-center py-3 min-h-screen'>Home Page</div>
+    <div className='text-center py-3 min-h-screen'>Welcome to Digital Event Manager</div>
   )
 }
 
